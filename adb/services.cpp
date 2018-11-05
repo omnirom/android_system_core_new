b7968aac0a0bfc807567866315b706c4bdcec6e4
Michael Bestas <mkbestas@lineageos.org>
2018-11-06 01:00:43 +0200

    adb: Rework adb root
    Change-Id: I33f67717a86246f7c3e66494ee67ac2b4fae90c0

--- adb/services.cpp
+++ adb/services.cpp
@@ -78,18 +78,13 @@ void restart_root_service(int fd, void *cookie) {
         WriteFdExactly(fd, "adbd is already running as root\n");
         adb_close(fd);
     } else {
-        if (!__android_log_is_debuggable()) {
-            WriteFdExactly(fd, "adbd cannot run as root in production builds\n");
-            adb_close(fd);
-            return;
-        }
-
-        int root_access = android::base::GetIntProperty("persist.sys.root_access", 0);
-        std::string build_type = android::base::GetProperty("ro.build.type", "");
-
-        if (build_type != "eng" && (root_access & 2) != 2) {
+        if (!allow_adb_root()) {
+#if defined(ALLOW_ADBD_ROOT)
             WriteFdExactly(fd, "root access is disabled by system setting - "
                     "enable in Settings -> System -> Developer options\n");
+#else
+            WriteFdExactly(fd, "adbd cannot run as root in production builds\n");
+#endif
             adb_close(fd);
             return;
         }
