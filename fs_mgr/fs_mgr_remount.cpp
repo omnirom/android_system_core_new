cd6522f375a4a3e5e73bfd7079b447892a9b6a86
Michael Bestas <mkbestas@lineageos.org>
2020-03-09 20:14:41 +0200

    fs_mgr: Bypass remount check
    Change-Id: I24bc8d54562981de6810cf5375120eb6a92b36b2

--- fs_mgr/fs_mgr_remount.cpp
+++ fs_mgr/fs_mgr_remount.cpp
@@ -127,7 +127,11 @@ int main(int argc, char* argv[]) {
     // If somehow this executable is delivered on a "user" build, it can
     // not function, so providing a clear message to the caller rather than
     // letting if fall through and provide a lot of confusing failure messages.
+#if defined(__ANDROID_RECOVERY__)
     if (!ALLOW_ADBD_DISABLE_VERITY || (android::base::GetProperty("ro.debuggable", "0") != "1")) {
+#else
+    if (!ALLOW_ADBD_DISABLE_VERITY) {
+#endif
         LOG(ERROR) << "only functions on userdebug or eng builds";
         return NOT_USERDEBUG;
     }
